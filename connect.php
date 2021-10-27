3c81ebf0d55f379b48d422f5c93d5f4b3cc97646
polylabs <hungnttg@gmail.com>
2021-10-27 11:23:49 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$host = "37.59.55.185";
+$username = "ZsPCk3T1Uu";
+$password = "nwHLKJsGGx";
+$database = "ZsPCk3T1Uu";
+
+$conn = mysqli_connect($host,$username,$password,$database);
+mysqli_query($conn,"SET NAMES 'utf8' ");
+?>
